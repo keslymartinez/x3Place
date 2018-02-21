3ed992b829043d32f9362b01eb071581252d0485
keslymartinez <keslymartinezvaldes@gmail.com>
2018-02-21 02:29:41 -0300

    cambiando a jquery y cierre de session

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-fetch('https://developers.zomato.com/api/v2.1/location_details?entity_id=36932&entity_type=group&apikey=986baeddd316fba15beaab45cfc24207')
+
 
 /**
 
@@ -28,9 +28,33 @@ var infoWindow = new google.maps.InfoWindow({map: map});
           // Browser doesn't support Geolocation
           handleLocationError(false, infoWindow, map.getCenter());
         }
-      **/
-// login de facebook 
+   **/ 
 
+  
+// modal para iniciar sesion 
+
+(function ($) {
+    $(function () {
+
+        //initialize all modals           
+        $('.modal').modal();
+
+        //now you can open modal from code
+        $('#modal1').modal('open');
+
+        //or by click on trigger
+        $('.trigger-modal').modal();
+
+    }); // end of document ready
+})(jQuery);
+
+
+
+
+
+
+// login de facebook 
+/**
 window.fbAsyncInit = function () {
     FB.init({
         appId: '229481407597435',
@@ -74,5 +98,106 @@ function login() {
         }
     });
 };
+
+
+**/
+
+
+
+$(function() {
+    var app_id = '229481407597435';
+    var scopes = 'email, user_friends, user_online_presence';
+    var btn_login = '<a href="#" id="login" class="btn btn-primary">Iniciar secion con Facebook</a>'
+    var div_session = "<div id='facebook-session'>";+
+                "<srtrong></strong>"+
+                "<img>"+
+                "<a href='#' id='logout' class='btn btn-danger'>Cerrar sesion</a>"+
+                "</div>";
+
+
+
+window.fbAsyncInit = function() {
+    FB.init({
+        appId: app_id,
+        status  :true,
+        cookie: true,
+        xfbml: true,
+        version: "v2.1"
+    });
+
+ FB.getLoginStatus(function(response) {
+    statusChangeCallback(response, function(){
+
+    });
+ });
+};
+ var statusChangeCallback = function(response, callback) {
+    console.log(response);
+
+    if(response.status === 'connected') {
+        getFacebookData();
+    }else {
+        callback(false);
+    }
+}
+
+    var checkLoginState = function(callback) {
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response, function(data) {
+        callback(data);
+        });
+    });
+   }
+   var getFacebookData = function()
+   { 
+        FB.api('/me', function(response){
+            $('#login').affter(div_session);
+            $('#login').remove();
+            $('#facebook-session strong').text("Bienvenido: "+response.name);
+            $('#facebook-session img').attr('src', 'http//graph.facebook.com/'+response.id+'/picture?type=large');
+        });
+   }
+
+   var facebookLogin = function(){
+    checkLoginState(function(response){
+        if(!response) {
+            FB.login(function(response){
+                if (response.status === 'connected')
+                    getFacebookData();
+            }, {scope: scopes})
+        }
+
+    })
+   }
+
+   var facebookLogout = function() {
+    FB.getLoginStatus(function(response){
+        if (response.status === 'connected') {
+            FB.logout(function(response){
+                $('#facebook-session').before(btn_login);
+                $('#facebook-session').remove();
+            })
+        }
+
+    });
+   }
+
+$(document).on('click', '#login', function(e) {
+    e.preventDefault();
+
+    facebookLogin();
+})
+
+$(document).on('click', '#logout', function(e) {
+    e.preventDefault();
+
+if(confirm("esta seguro?"))
+    facebookLogout();
+else
+    return false;
+})
+
+
+})
 
 
